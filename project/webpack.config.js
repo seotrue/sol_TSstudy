a5537a28e55e480913cf8bbe47ba5885a3ac22a0
sjs <seo777111@gmail.com>
2021-09-01 20:10:57 +0900

    complete final project section3

---
+++ project/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  output: {
+    filename: 'app_sol.ts',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new webpack.ProgressPlugin(), new CleanWebpackPlugin()],
+};
